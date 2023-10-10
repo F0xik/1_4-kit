7842910017da2d94cf774e94f9efb740bee541a5
tanas <tanas@FOXIK-PC>
2023-10-10 18:49:45 +0300

    Добавьте файлы проекта.

---
+++ 1_4/Program.cs
@@ -0,0 +1,5 @@
+﻿double price, discount; int count;
+Console.WriteLine("Введите стоимость товара:"); price = Convert.ToDouble(Console.ReadLine()); // Ввод стоимости товара
+Console.WriteLine("Укажите количество: "); count = Convert.ToInt32(Console.ReadLine()); // Ввод количества
+Console.WriteLine("Укажите скидку: "); discount = Convert.ToDouble(Console.ReadLine()); // Ввод скидки
+Console.WriteLine($"Итоговая стоимость с учетом скидки {discount}% составляет: {(price * count) - ((price * count) / 100 * discount)}");
